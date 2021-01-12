a3af29f689859e81d3a1ff901b343110a7b66a0c
Cicero <ciceronascimentu@gmail.com>
2021-01-11 21:04:42 -0300

    Eager Loading

--- ControllerSales/Services/SellerService.cs
+++ ControllerSales/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ControllerSales.Data;
 using ControllerSales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ControllerSales.Services
 {
@@ -28,7 +29,7 @@ namespace ControllerSales.Services
 
         public Seller FindById (int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove (int id)
